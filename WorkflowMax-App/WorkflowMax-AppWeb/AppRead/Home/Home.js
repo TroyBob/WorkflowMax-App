8389c6fd3ade818eb16ea43ee829ba4da0333109
Troy Cumpsty <troy@becloudsmart.com>
2015-11-25 16:36:22 +0800

    Added in upload timesheet functionality with attached notes.

--- WorkflowMax-App/WorkflowMax-AppWeb/AppRead/Home/Home.js
+++ WorkflowMax-App/WorkflowMax-AppWeb/AppRead/Home/Home.js
@@ -13,7 +13,9 @@
 
             runApp(sender_email);
 
-            displayItemDetails();
+            $("#uploadNote").click(uploadNote);
+            $("#uploadTimesheet").click(uploadTimesheet);
+
         });
     };
 
@@ -38,10 +40,70 @@
 
         document.getElementById("Current").innerHTML = "<b>Company ID : </b>" + returnID;
 
-        printJobs(returnID);
+        printJobs(returnID);    
+    }
+
+    function uploadNote()
+    {
+        var jobs = document.getElementById("Jobs").options.selectedIndex;
+        var currentJob = document.getElementById("Jobs").options[jobs].text;
+        var noteText = document.getElementById("Note").value;
+
+        var apicall = "https://api.workflowmax.com/job.api/note?apiKey=14C10292983D48CE86E1AA1FE0F8DDFE&accountKey=8A39F28D022B4366975D6FCDB180C839";
+
+        var note = "<Note><Job>" + currentJob + "</Job><Title>Email note</Title><Text>" + noteText + "</Text></Note>";
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('POST', apicall);
+
+        xhr.send(note);
+    }
+
+    function uploadTimesheet()
+    {
+        var jobs = document.getElementById("Jobs").options.selectedIndex;
+        var currentJob = document.getElementById("Jobs").options[jobs].text;
+        var noteText = document.getElementById("Note").value;
+
+        var task = getTask(currentJob);
+
+        var apicall = "https://api.workflowmax.com/time.api/add?apiKey=14C10292983D48CE86E1AA1FE0F8DDFE&accountKey=8A39F28D022B4366975D6FCDB180C839";
+        var tsxml = "<Timesheet><Job>"+ currentJob + "</Job><Task>36465645</Task><Staff>304191</Staff><Date>20151125</Date><Minutes>15</Minutes><Note>" + noteText + "</Note></Timesheet>";
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('POST', apicall);
+
+        xhr.send(tsxml);
+
+
+
+
+
+    }
+
+    function getStaff()
+    {
+
+    }
+
+    function getTask(jobID)
+    {
+        var apicall = "https://api.workflowmax.com/job.api/get/" + jobID + "?apiKey=14C10292983D48CE86E1AA1FE0F8DDFE&accountKey=8A39F28D022B4366975D6FCDB180C839";
+
+        var jobDetails = getXML(apicall);
+
+        var tasklist = jobDetails.getElementsByTagName("Task");
+
+        var thistask = tasklist[0].getElementsByTagName("Name").childNodes[0].nodeValue;
+
+        document.getElementById("Email").innerHTML = "<b>Email : boo! </b> ";
 
         
     }
+
+
 
     function getXML(list)
     {
@@ -55,7 +117,7 @@
 
     function getID(passedXML, emailString)
     {
-        var clientNum = passedXML.getElementsByTagName("Client"); // returns the number of clients
+        var clientNum = passedXML.getElementsByTagName("Client"); 
 
         for (var i = 0; i < clientNum.length; i++)
         {
@@ -106,9 +168,6 @@
     function printTasks(jobID)
     {
         var dropdown = "";
-        var taskList = "
-
-
-
     }
+
 })();
